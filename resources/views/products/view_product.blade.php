6ad5e62a2fb4b8163092d2363a2125434f4dfefe
AvishkaBandara <avishkabandara2001@gmail.com>
2025-06-14 09:12:21 -0700

    view and sale product Ui done

--- resources/views/products/view_product.blade.php
+++ resources/views/products/view_product.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'View Product')
 
 @section('content')
-  <div class="container pt-4 col-lg-10 col-md-10 col-sm-6">
-    <div class="row mb-4 pt-4">
+    <div class="container pt-4 col-lg-10 col-md-10 col-sm-6">
+        <div class="row mb-4 pt-4">
             <div class="col-lg-12 col-md-10 col-sm-6">
                 <h1 class="h3">View Product</h1>
             </div>
-    </div>
-    <div class="row justify-content-center pb-5">
+        </div>
+        <div class="row justify-content-center pb-5">
             <div class="col-lg-12 col-md-10 col-sm-6">
                 <div class="card shadow">
                     <div class="card-body">
@@ -30,22 +30,20 @@
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <div class="btn-group col-md-4 mt-3">
-                                <a class="btn btn-secondary dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown link
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </div>
+                                <div class="col-md-6">
+                                    <label for="product" class="form-label">Select Product</label>
+                                    <input class="form-control" list="productList" id="product" name="product"
+                                        placeholder="Type to search..." style="box-shadow: none;">
+                                    <datalist id="productList">
+                                        {{-- @foreach ($products as $product)
+                                            <option value="{{ $product->name }} ({{ $product->brand }}) - ${{ number_format($product->price, 2) }}">
+                                        @endforeach --}}
+                                    </datalist>
+                                </div>
 
                             </div>
 
-                            
+
 
                             <div class="text-end mt-5">
                                 <button type="submit" class="btn btn-success col-3">
@@ -59,42 +57,42 @@
             </div>
         </div>
 
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Products</h6>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Brand</th>
-                            <th>Available quantity</th>
-                            <th>Expire Date</th>
-                            <th>Sale Quantity</th>
-                            <th>Price</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
-                            <td>2011/04/25</td>
-                            <td>$320,800</td>
-                            <td>
-                                <a href="#" class="btn btn-primary btn-sm">
-                                    <i class="fas fa-eye"></i> View
-                                </a>
-                        </tr>
-                    </tbody>
-                </table>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Products</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Brand</th>
+                                <th>Available quantity</th>
+                                <th>Expire Date</th>
+                                <th>Sale Quantity</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Tiger Nixon</td>
+                                <td>System Architect</td>
+                                <td>Edinburgh</td>
+                                <td>61</td>
+                                <td>2011/04/25</td>
+                                <td>$320,800</td>
+                                <td>
+                                    <a href="#" class="btn btn-primary btn-sm">
+                                        <i class="fas fa-eye"></i> View
+                                    </a>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 
 
--- resources/views/sales/sales.blade.php
+++ resources/views/sales/sales.blade.php
@@ -1,8 +1,86 @@
 @extends('layouts.layouts')
 
 @section('title', 'Sales')
+
+
 @section('content')
 
+    @vite(['resources/css/sales.css'])
+    @vite(['resources/js/sales/sales.js'])
+
+
+    <div class="container pt-4 col-lg-10 col-md-10 col-sm-6">
+        <h4 class="mb-4">New Bill</h4>
+
+        <!-- Product Selection Row -->
+        <form action="#" method="POST" id="productForm">
+        <div class="row g-3 align-items-end px-3" style="display: flex; justify-content: space-between;">
+            <div class="col-md-4">
+                <label for="product" class="form-label">Select Product</label>
+                <input class="form-control" list="productList" id="product" name="product" placeholder="Type to search..." style="box-shadow: none;">
+                <datalist id="productList">
+                    {{-- @foreach ($products as $product)
+                        <option
+                            value="{{ $product->name }} ({{ $product->brand }}) - ${{ number_format($product->price, 2) }}">
+                    @endforeach --}}
+                </datalist>
+            </div>
+
+            <div class="col-md-3">
+                <label for="quantity" class="form-label">Qty</label>
+                <input type="number" class="form-control" id="quantity" name="quantity" min="1" value="1">
+            </div>
+
+            <div class="col-md-2">
+                <button class="btn btn-success w-100" type="submit">Add</button>
+            </div>
+        </div>
+        </form>
+        <!-- Bill Table -->
+        <div class="card shadow mb-4 mt-3">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Product</th>
+                                <th>Qty</th>
+                                <th>Price ($)</th>
+                                <th>Total ($)</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Sample row (static) -->
+                            <tr>
+                                <td>1</td>
+                                <td>Sample Product</td>
+                                <td>2</td>
+                                <td>10.00</td>
+                                <td>20.00</td>
+                                <td><button class="btn btn-sm btn-danger">X</button></td>
+                            </tr>
+                        </tbody>
+                        <tfoot class="table-light">
+                            <tr>
+                                <th colspan="4" class="text-end">Grand Total:</th>
+                                <th>$20.00</th>
+                                <th></th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <!-- Print Button -->
+        <div class="text-end mt-3">
+            <button class="btn btn-primary">
+                <i class="fas fa-print me-1"></i> Print Bill
+            </button>
+        </div>
+    </div>
 
 
 @endsection
