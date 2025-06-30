9885150dade3f09e4e5f453b67f162090cb81d12
AvishkaBandara <avishkabandara2001@gmail.com>
2025-06-30 22:38:27 +0530

    sphatie imported

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -24,6 +26,7 @@ class User extends Authenticatable
         'role',
         'email',
         'password',
+        'role_id',
     ];
 
     /**
@@ -48,4 +51,5 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
 }
